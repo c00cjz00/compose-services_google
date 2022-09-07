e9cfec124b576b5f2ec2c2d2e002874f3e769a1f
Brian Walsh <brian@bwalsh.com>
2022-09-07 15:39:06 -0700

    Refactor array config

--- guppy_setup.sh
+++ guppy_setup.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 # Script to create and re-create es indices and setup guppy
 
-sleep 2
-docker exec esproxy-service curl -X DELETE http://localhost:9200/etl_0
-sleep 2
-docker exec esproxy-service curl -X DELETE http://localhost:9200/file_0
-sleep 2
-docker exec esproxy-service curl -X DELETE http://localhost:9200/file-array-config_0
-sleep 2
-docker exec esproxy-service curl -X DELETE http://localhost:9200/etl-array-config_0
-sleep 2
-docker exec tube-service bash -c "python run_config.py && python run_etl.py"
+docker exec esproxy-service curl -X DELETE http://localhost:9200/gen3.aced.*
+docker-compose stop guppy-service
+docker-compose rm -f guppy-service
 
-docker container stop guppy-service
-docker container start guppy-service
+./etl/tube_lite --credentials_path $1  --elastic http://localhost:9200
+
+docker-compose up -d guppy-service
