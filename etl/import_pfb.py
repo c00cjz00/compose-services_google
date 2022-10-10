5c72d07ea5bf64083c3f4390b71004aeb60186b9
Brian Walsh <brian@bwalsh.com>
2022-10-10 15:50:01 -0700

    link object_id from indexd to DocumentReference

--- etl/import_pfb.py
+++ etl/import_pfb.py
@@ -65,14 +65,15 @@ def get_ids_from_table(db, table, ids, id_column):
     return data if data and data.first() else None
 
 
-def convert_to_node(x, is_base64):
+def convert_to_node(x, is_base64, project_id):
     obj = x["object"]
     to_update = {}
     for name, value in obj.items():
-        if value and is_base64[x["name"]][name]:
-            to_update[name] = decode_enum(value)
+        if name in is_base64[x["name"]]:
+            if value and is_base64[x["name"]][name]:
+                to_update[name] = decode_enum(value)
     # TODO get parameter from CLI
-    to_update['project_id'] = "MyFirstProgram-MyFirstProject"
+    to_update['project_id'] = project_id
     obj.update(to_update)
 
     r = {
@@ -103,20 +104,23 @@ def convert_to_edge(x, edge_tables):
     ]
 
 
-def write_table(cur, table, node):
+def write_table(cur, table, node, dry_run=True):
     """Write node to table."""
-    # print(table, node)
+    # TODO - improve speed https://naysan.ca/2020/05/09/pandas-to-postgresql-using-psycopg2-bulk-insert-performance-benchmark/
     columns = ','.join(node.keys())
     place_holders = ','.join(['%s'] * len(node.keys()))
     values = [node[column] for column in node.keys()]
     if 'node_id' in node.keys():
+        # vertex
         insert = f'insert into "{table}" ({columns}) values ({place_holders}) on conflict(node_id) do nothing;'
     else:
+        # edge
         insert = f'insert into "{table}" ({columns}) values ({place_holders}) on conflict(src_id, dst_id) do nothing;'
-    cur.execute(SQL(insert), values)
+    if not dry_run:
+        cur.execute(SQL(insert), values)
 
 
-def import_pfb_job(pfb_file, ddt, conn):
+def import_pfb_job(pfb_file, project_id, project_node_id, ddt, conn, dry_run, document_reference_object_ids):
     """Import the PFB into the database."""
     start_time = datetime.now()
     print(start_time)
@@ -143,7 +147,6 @@ def import_pfb_job(pfb_file, ddt, conn):
             for field in node["fields"]:
                 fields[field["name"]] = is_enum(field["type"])
 
-        mode = "append"
         edge_table_by_labels = ddt.get_edge_table_by_labels()
 
         insert_count = 0
@@ -154,11 +157,20 @@ def import_pfb_job(pfb_file, ddt, conn):
             if record['name'] == 'Metadata':
                 continue
             if record['name'] == 'ResearchStudy':
-                record['relations'] = [{"dst_id": "d8668bf7-ed94-5fe2-bc2c-24a9ede57d4f", "dst_name": "project"}]
+                # link the ResearchStudy to the gen3 project
+                record['relations'] = [{"dst_id": project_node_id, "dst_name": "project"}]
+            if record['name'] == 'DocumentReference':
+                # link the ResearchStudy to the gen3 project
+                if record['id'] in document_reference_object_ids:
+                    record['object']['object_id'] = document_reference_object_ids[record['id']]
+                    # print(f"Set document_reference_object_ids {record['object']['object_id']}")
+
             write_table(
                 cur=cur,
                 table=ddt.get_node_table_by_label()[record['name']],
-                node=convert_to_node(record, _is_base64))
+                node=convert_to_node(record, _is_base64, project_id),
+                dry_run=dry_run
+            )
             edge_tuples = convert_to_edge(record, edge_table_by_labels)
             if edge_tuples:
                 for edge_tuple in edge_tuples:
@@ -167,7 +179,9 @@ def import_pfb_job(pfb_file, ddt, conn):
                     write_table(
                         cur=cur,
                         table=edge_table,
-                        node=edge)
+                        node=edge,
+                        dry_run=dry_run
+                    )
             insert_count += 1
             if insert_count == batch_size:
                 conn.commit()
@@ -184,14 +198,13 @@ def import_pfb_job(pfb_file, ddt, conn):
 
 
 if __name__ == "__main__":
-    # access_token = os.environ["ACCESS_TOKEN"]
-    # hostname = os.environ["GEN3_HOSTNAME"]
-    # input_data = os.environ["INPUT_DATA"]
 
-    # input_data_json = json.loads(input_data)
-
+    # TODO - get this from the command line
+    dry_run = False
+    # TODO - get this from the command line
     dictionary_url = 'https://aced-public.s3.us-west-2.amazonaws.com/aced.json'  # os.environ["DICTIONARY_URL"]
 
+    # TODO - get this from the command line
     with open("Secrets/sheepdog_creds.json") as pelican_creds_file:
         sheepdog_creds = json.load(pelican_creds_file)
 
@@ -201,48 +214,58 @@ if __name__ == "__main__":
     DB_USER = sheepdog_creds["db_username"]
     DB_PASS = sheepdog_creds["db_password"]
 
+    # TODO - get this from the command line
     DB_NAME = 'metadata_db'
+
+    # TODO - get this from the command line
+    program_name = 'MyFirstProgram'
+    project_code = 'MyFirstProject'
+
     # NEW_DB_NAME = input_data_json["db"]
 
     conn = psycopg2.connect(
         database=DB_NAME,
         user=DB_USER,
         password=DB_PASS,
+        # TODO - get this from the command line
         host='localhost',
         # port=DATABASE_CONFIG.get('port'),
     )
 
-    # # create a database in the name that was passed through
-    # engine = sqlalchemy.create_engine(
-    #     "postgresql://{user}:{password}@{host}/metadata_db".format(
-    #         user=DB_USER, password=DB_PASS, host='localhost'  # sheepdog_creds["db_host"]
-    #     )
-    # )
-    # conn = engine.connect()
+    cur = conn.cursor()
+    cur.execute("select node_id, _props from \"node_program\";")
+    programs = cur.fetchall()
+    programs = [{'node_id': p[0], '_props': p[1]} for p in programs]
+    program = next(iter([p for p in programs if p['_props']['name'] == program_name]), None)
+    assert program, f"{program_name} not found in node_program"
+    cur.execute("select node_id, _props from \"node_project\";")
+    projects = cur.fetchall()
+    projects = [{'node_id': p[0], '_props': p[1]} for p in projects]
+    project_node_id = next(iter([p['node_id'] for p in projects if p['_props']['code'] == project_code]), None)
+    assert project_node_id, f"{project_code} not found in node_project"
+    project_id = f"{program_name}-{project_code}"
 
-    # conn.execute("commit")
-    #
-    # print("we are creating a new database named ", NEW_DB_NAME)
-    #
-    # create_db_command = text("create database :db")
-    # print("This is the db create command: ", create_db_command)
-    #
-    # grant_db_access = text("grant all on database :db to sheepdog with grant option")
-    # print("This is the db access command: ", grant_db_access)
-    #
-    # try:
-    #     conn.execute(create_db_command, db=NEW_DB_NAME)
-    #     conn.execute(grant_db_access, db=NEW_DB_NAME)
-    # except Exception as e:
-    #     print("Unable to create database", e)
-    #     raise Exception
-    #
-    # conn.close()
-
-    # DB_URL = "jdbc:postgresql://{}/{}".format(sheepdog_creds["db_host"], NEW_DB_NAME)
+    # TODO - get this from the command line
+    pfb_file = 'output/research_study_Alzheimers.pfb'
+    print(f"Importing {pfb_file} into {project_id} project node {project_node_id}")
 
     dictionary, model = init_dictionary(url=dictionary_url)
     ddt = DataDictionaryTraversal(model)
 
-    name = '../data_model/research_study_Alzheimers.pfb'
-    import_pfb_job(name, ddt, conn)
+    # TODO - get this from the command line
+    # get the object ids created by file upload-pfb
+    document_reference_object_ids = {}
+    with open('file-object_ids.ndjson') as f:
+        for line in f.readlines():
+            document_reference_object_id = json.loads(line)
+            document_reference_object_ids[document_reference_object_id['id']] = document_reference_object_id['object_id']
+
+    import_pfb_job(
+        pfb_file=pfb_file,
+        project_id=project_id,
+        project_node_id=project_node_id,
+        ddt=ddt,
+        conn=conn,
+        dry_run=dry_run,
+        document_reference_object_ids=document_reference_object_ids
+    )
