6854fd4196e9fc57b40781c614d6f11a3133408f
Brian Walsh <brian@bwalsh.com>
2022-10-20 10:36:58 -0700

    lower case tables

--- etl/truncate_imported_tables.sql
+++ etl/truncate_imported_tables.sql
@@ -1,21 +1,23 @@
 -- delete all imported data
 
 \c metadata_db ;
-
-TRUNCATE	"node_Condition"	CASCADE ;
-TRUNCATE	"node_DiagnosticReport"	CASCADE ;
-TRUNCATE	"node_DocumentReference"	CASCADE ;
-TRUNCATE	"node_Encounter"	CASCADE ;
-TRUNCATE	"node_ImagingStudy"	CASCADE ;
-TRUNCATE	"node_Location"	CASCADE ;
-TRUNCATE	"node_Medication"	CASCADE ;
-TRUNCATE	"node_MedicationRequest"	CASCADE ;
-TRUNCATE	"node_Observation"	CASCADE ;
-TRUNCATE	"node_Organization"	CASCADE ;
-TRUNCATE	"node_Patient"	CASCADE ;
-TRUNCATE	"node_Practitioner"	CASCADE ;
-TRUNCATE	"node_Procedure"	CASCADE ;
-TRUNCATE	"node_ResearchStudy"	CASCADE ;
-TRUNCATE	"node_ResearchSubject"	CASCADE ;
-TRUNCATE	"node_Specimen"	CASCADE ;
-TRUNCATE	"node_Task"	CASCADE ;
+TRUNCATE        "node_condition"       CASCADE ;
+TRUNCATE        "node_datarelease"     CASCADE ;
+TRUNCATE        "node_diagnosticreport"        CASCADE ;
+TRUNCATE        "node_documentreference"       CASCADE ;
+TRUNCATE        "node_encounter"        CASCADE ;
+TRUNCATE        "node_imagingstudy"     CASCADE ;
+TRUNCATE        "node_location"         CASCADE ;
+TRUNCATE        "node_medication"       CASCADE ;
+TRUNCATE        "node_medicationrequest"       CASCADE ;
+TRUNCATE        "node_observation"     CASCADE ;
+TRUNCATE        "node_organization"     CASCADE ;
+TRUNCATE        "node_patient"  CASCADE ;
+TRUNCATE        "node_practitioner"     CASCADE ;
+TRUNCATE        "node_practitionerrole"         CASCADE ;
+TRUNCATE        "node_procedure"         CASCADE ;
+TRUNCATE        "node_researchstudy"   CASCADE ;
+TRUNCATE        "node_researchsubject"         CASCADE ;
+TRUNCATE        "node_root"     CASCADE ;
+TRUNCATE        "node_specimen"         CASCADE ;
+TRUNCATE        "node_task"    CASCADE ;
