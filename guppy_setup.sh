c94e10f676a250dcc3ae59b989409e774d0535b5
Brian Walsh <brian@bwalsh.com>
2022-10-27 10:30:19 -0700

    Adds batch size

--- guppy_setup.sh
+++ guppy_setup.sh
@@ -5,6 +5,6 @@ docker exec esproxy-service curl -X DELETE http://localhost:9200/gen3.aced.*
 docker-compose stop guppy-service
 docker-compose rm -f guppy-service
 
-./etl/tube_lite --credentials_path $1  --elastic http://localhost:9200
+./etl/tube_lite --credentials_path $1  --elastic http://localhost:9200  --batch_size 20   #  --limit 1000
 
 docker-compose up -d guppy-service
