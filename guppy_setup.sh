185089387127e8977512f0be7a2a039f2fd83624
Brian Walsh <brian@bwalsh.com>
2022-10-31 14:21:36 -0700

    Improves throughput

--- guppy_setup.sh
+++ guppy_setup.sh
@@ -5,6 +5,7 @@ docker exec esproxy-service curl -X DELETE http://localhost:9200/gen3.aced.*
 docker-compose stop guppy-service
 docker-compose rm -f guppy-service
 
-./etl/tube_lite --credentials_path $1  --elastic http://localhost:9200  --batch_size 20   #  --limit 1000
+# start two processes one for observation and one for file, run them in parallel at a higher 'niceness' level
+echo -e observation"\n"file  | nice -n 10 xargs -L 1 -P 2 nice -n 10 ./etl/tube_lite --credentials_path $1  --elastic http://localhost:9200  --batch_size 20  --entity
 
 docker-compose up -d guppy-service
