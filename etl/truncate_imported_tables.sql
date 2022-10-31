61326af5fa580841d7706001fc97d9e785b7521a
Brian Walsh <brian@bwalsh.com>
2022-10-31 14:17:11 -0700

    Adds indexd truncate

--- etl/truncate_imported_tables.sql
+++ etl/truncate_imported_tables.sql
@@ -21,3 +21,12 @@ TRUNCATE        "node_researchsubject"         CASCADE ;
 TRUNCATE        "node_root"     CASCADE ;
 TRUNCATE        "node_specimen"         CASCADE ;
 TRUNCATE        "node_task"    CASCADE ;
+
+\c  indexd_db; 
+TRUNCATE 	"base_version"	CASCADE ;
+TRUNCATE 	"index_record"	CASCADE ;
+TRUNCATE 	"index_record_authz"	CASCADE ;
+TRUNCATE 	"index_record_hash"	CASCADE ;
+TRUNCATE 	"index_record_metadata"	CASCADE ;
+TRUNCATE 	"index_record_url"	CASCADE ;
+
